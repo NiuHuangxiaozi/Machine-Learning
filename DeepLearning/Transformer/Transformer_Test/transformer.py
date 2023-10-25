f18d6e9f5579e18fb3554e0f2595343436128ae9
Niu <100760855+NiuHuangxiaozi@users.noreply.github.com>
2023-10-25 20:22:17 +0800

    Update transformer.py

--- DeepLearning/Transformer/Transformer_Test/transformer.py
+++ DeepLearning/Transformer/Transformer_Test/transformer.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from torch import nn, optim
 from string import punctuation
 from collections import Counter
+#nltk是一个自然语言处理库
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from sklearn.model_selection import train_test_split
@@ -15,7 +16,7 @@ import os
 import torch.nn.functional as F
 
 
-
+#多头注意力机制
 class Self_Attention(nn.Module):
     
     def __init__(self,atten_mask,input_size=512,hidden_size=512,heads=8):
@@ -116,7 +117,8 @@ class Self_Attention(nn.Module):
         result=torch.cat(output,dim=2)
         
         return result
-    
+
+#层归一化的实现
 class LayerNorm(nn.Module):
     
     def __init__(self,eps=1e-05,affine=True):
@@ -164,7 +166,9 @@ class LayerNorm(nn.Module):
         else:
             self._y=torch.ones((L,C),requires_grad=False).to(self.device)
             self._b=torch.zeros((L,C),requires_grad=False).to(self.device)
-            
+
+
+#FNN层，就是两层线性层
 class FeedForward(nn.Module):
     def __init__(self,embedding_size,hiddensize,dropout=0.1):
         super(FeedForward,self).__init__()
@@ -250,7 +254,8 @@ class DecoderLayer(nn.Module):
         de_input=self.layernormalization2(de_input+self.mask_self_attention_1(en_input,en_input,de_input))
         de_input=self.layernormalization3(de_input+self.feed(de_input))    
         return de_input
-        
+
+#这是我自己实现的Transformer模型
 class Transformer(nn.Module):
     def __init__(self,LayerNumber,HiddenSize,Heads,Mask_matrix):
         super(Transformer,self).__init__()
@@ -268,12 +273,15 @@ class Transformer(nn.Module):
         
         self.device=None
     def forward(self,inputs,sr_outputs):
-        
+
+        #输入的inputs在哪个设备上，我们就将模型内部的局部变量加载到相应设备上
         self.device=inputs.device
-            
+
+        #添加位置编码
         inputs=inputs+self.Generate_positional_matrix(inputs)
         sr_outputs=sr_outputs+self.Generate_positional_matrix(sr_outputs)
-        
+
+        #多层EncoderLayer和Dencoderlayer的叠加
         en_vals=inputs
         de_vals=sr_outputs
         for layer in range(self.LayerNumber):
@@ -309,23 +317,30 @@ def generate_mask(L):
         mask=torch.triu(torch.full((L,L),1),diagonal=1)
         return mask
     
-    
+
+#为了适用本次的任务设计的模型，在Transformer后面加上了softmax层
 class Sen_model(nn.Module):
     def __init__(self,input_dim,hidden_dim,transformer_layer,heads,mask,classification,seq_length):
         super(Sen_model,self).__init__()
         
-        self.input_dim=input_dim
-        self.hidden_dim=hidden_dim
-        self.seq_length=seq_length
-        self.layer=transformer_layer
-        self.heads=heads
+        self.input_dim=input_dim #输入词向量的维度
+        self.hidden_dim=hidden_dim #transformer里面隐层的大小
+        self.seq_length=seq_length #句子的长度
+        self.layer=transformer_layer #transformer里面encoder和decoder叠加的层数
+        self.heads=heads #多头注意力机制的头数
+        self.mask=mask  #decoder使用的mask矩阵
+        self.classification=classification #最后softmax分类的个数，这里是5
+
+
         
-        self.mask=mask
-        self.classification=classification
-        
+        #首先一个线性层，将词向量的维度映射到transformer里面隐层的大小
         self.linear1=nn.Linear(in_features=self.input_dim,out_features=self.hidden_dim)
+        
         self.transformer=Transformer(LayerNumber=self.layer,HiddenSize=self.hidden_dim,Heads=self.heads,Mask_matrix=self.mask)
+        #进行分类的linear
         self.linear2=nn.Linear(in_features=self.hidden_dim*self.seq_length,out_features=self.classification)
+
+    
     def forward(self,x):
         x=self.linear1(x)
         x=self.transformer(x,x)
@@ -336,25 +351,29 @@ class Sen_model(nn.Module):
     
 
 
-
+'''
+这个类是训练数据的准备类，用于产生训练集、测试机和验证集
+这个类的实现方法在Kaggle中 ，网址为https://www.kaggle.com/code/vikkach/sentiment-analysis-lstm-pytorch 
+可以去看他的解释，比较详细。我只是搬运理解的打工人。
+'''
 class Data_prepare:
     def __init__(self,dir_path,train_batch,test_batch):
         
-        self.dir_path=dir_path
-        self.category=5
-        self.train_batch=train_batch
-        self.test_batch=test_batch
+        self.dir_path=dir_path #csv文件在的文件夹路径
+        self.category=5 #最后分类的个数
+        self.train_batch=train_batch #训练batchsize
+        self.test_batch=test_batch #测试batchsize
         
-        self.train_data=None
-        self.valid_data=None
-        self.test_data=None
-        self.train_loader =None
-        self.valid_loader =None
-        self.test_loader=None
+        self.train_data=None #训练数据集
+        self.valid_data=None #验证数据集
+        self.test_data=None #测试数据集
+        self.train_loader =None #训练数据集的数据加载器
+        self.valid_loader =None #训练数据集的数据加载器
+        self.test_loader=None #训练数据集的数据加载器
         
         
         
-        self.encoded_voc=None
+        self.encoded_voc=None 
         self.train_review_lens=None
         self.test_zero_idx=None
         
@@ -363,60 +382,72 @@ class Data_prepare:
         
         
     def Prepare(self):
-        
+        #使用pandas库的read_csv读取文件
         train_data = pd.read_csv(self.dir_path+'/train.tsv', sep = '\t')
         test_data = pd.read_csv(self.dir_path+'/test.tsv', sep = '\t')
-        
+        #去除有没有Nan的值
         test_data['Phrase'] = test_data['Phrase'].fillna(" ")
         
-        #assert(test_data.isnull().sum()==0)
         
+        #对输入的训练数据和测试数据集进行预处理，主要做的就是 ：转化为小写；提取单词主干和去除停用词
         train_data_pp = self.pre_process(train_data)
         test_data_pp = self.pre_process(test_data)
-        
         print('Phrase before pre-processing: ', train_data['Phrase'][0])
         print('Phrase after pre-processing: ', train_data_pp[0])
+
         
-        
+        #为所有的单词确定一个唯一的序号，依据是：词语频率
         self.encoded_voc = self.encode_words(train_data_pp + test_data_pp)
         
+        
+        将原来的语句单词转化为数字的表示形式，依靠前面train_data_pp和test_data_pp字典
         train_reviews_ints = self.encode_data(train_data_pp)
         test_reviews_ints = self.encode_data(test_data_pp)
         print('Example of encoded train data: ', train_reviews_ints[0])
         print('Example of encoded test data: ', test_reviews_ints[0])
         
-        
-        
+
+        #由1，2，3变为one-hot编码
         y_target = self.to_categorical(train_data['Sentiment'],self.category)
         print('Example of target: ', y_target[0])
         
+
+        #这里所有句子长度的一个数量统计，返回一个字典，主要是后面为了确定最大的句子长度，用来padding
         self.train_review_lens = Counter([len(x) for x in train_reviews_ints])
         test_review_lens = Counter([len(x) for x in test_reviews_ints])
-        
+
+
+        #在这里我们记录下长度为0的句子的下标，后面在输出情感的时候直接输出：中立
         self.test_zero_idx = [test_data.iloc[ii]['PhraseId'] for ii, review in enumerate(test_reviews_ints) if len(review) == 0]
         # remove reviews with 0 length
         non_zero_idx = [ii for ii, review in enumerate(train_reviews_ints) if len(review) != 0]
 
+
+        #这里将训练数据集中句子长度已经为0的句子删除
         train_reviews_ints = [train_reviews_ints[ii] for ii in non_zero_idx]
         y_target = np.array([y_target[ii] for ii in non_zero_idx])
-
         print('Number of reviews after removing outliers: ', len(train_reviews_ints))
         
-        
+
+        #根据最大的句子长度进行padding
         train_features = self.pad_features(train_reviews_ints, max(self.train_review_lens))
         X_test = self.pad_features(test_reviews_ints, max(self.train_review_lens))
 
+
+        #在train训练集中划分验证集
         X_train,X_val,y_train,y_val = train_test_split(train_features,y_target,test_size = 0.2)
         print(X_train[0])
         print(y_train[0])
-
         print("X_train",X_train.shape)
         print("X_val",X_val.shape)
         print("X_test",X_test.shape)
-        
+
+        #提取出测试集的index后面测试有用
         ids_test = np.array([t['PhraseId'] for ii, t in test_data.iterrows()])
         print(ids_test)
-        
+
+
+        #下面就是pytorch经典生成数据加载器的过程
         self.train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
         self.valid_data = TensorDataset(torch.from_numpy(X_val), torch.from_numpy(y_val))
         self.test_data = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(ids_test))
@@ -511,8 +542,12 @@ class Data_prepare:
             return self.test_loader
     def Get_vali_loader(self):
             return self.valid_loader
+
+    #获得句子的最大长度
     def Get_max_embedding_size(self):
         return max(self.train_review_lens)
+        
+    #获得test集合中句子长度为0的index集合返回出来
     def Get_test_zero_index(self):
         return self.test_zero_idx
       
@@ -521,57 +556,65 @@ class Data_prepare:
 if __name__=='__main__':
     
     #定义一些超参数
-    device =torch.device("cuda") if torch.cuda.is_available() else torch.device('cpu') 
-    train_batch=128
-    test_batch=128
-    mask_matrix=generate_mask(30).to(device)
-    learning_rate=1e-3
-    Epoch=2
+    device =torch.device("cuda") if torch.cuda.is_available() else torch.device('cpu') #训练设备
+    train_batch=128 #训练batchsize
+    test_batch=128  #测试batchsize
+    mask_matrix=generate_mask(30).to(device) #生成decoder的mask矩阵
+    learning_rate=1e-3 #学习率
+    Epoch=2 #训练轮数
     path=os.getcwd()
-    
-    print("Data preparation begin!")
+
+
+    #加载数据
+    print("Data preparation begin!")#
     pData=Data_prepare(path,train_batch,test_batch)
     train_loader=pData.Get_train_loader()
     test_loader=pData.Get_test_loader()
     vali_loader=pData.Get_vali_loader()
     print("Data preparation end!")
+
+
     
     print("Train model begin")
-    #训练准备
+    #训练准备，定义模型
     model=Sen_model(input_dim=1,hidden_dim=256,transformer_layer=2,heads=8,mask=mask_matrix,classification=5,seq_length=30)
+    #将模型记载到相应设备上
     model = model.to(device)
+    #定义优化器
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    #定义损失函数
     criterion = nn.CrossEntropyLoss()
 
     #history记录test阶段的loss和accurary用于画图
     history = {'Test Loss':[],'Test Accuracy':[]}
-    
+
+    #开始多轮训练
     for epoch in range(1,Epoch+1):
-       #训练部分
         '''
             这个写法可以生成进度条，enumerrate会生成（index，value)的组合对
             trainloader对象本来就是（data，label）的组合对
         '''     
         processBar = tqdm(enumerate(train_loader), total=len(train_loader))
-            #模型训练之前一定要写
+        #模型训练之前一定要写
         model.train()
-
 
         for index,(data,label) in processBar:
             data=data.reshape(data.shape[0],data.shape[1],1).float().to(device)
             label=label.to(device)
 
-                #模型前向传播
+            #模型前向传播
             outputs=model(data)
 
-                #argmax就是按照某一个维度求得这个维度上最大值的下标，如果不想降维，请使用keepdim=True
+            #argmax就是按照某一个维度求得这个维度上最大值的下标，如果不想降维，请使用keepdim=True
             prediction=torch.argmax(outputs,dim=1)
 
+            #使用argmax将one-hot变为index类型，便于下面算正确率
             label_index=torch.argmax(label, dim=1)
-                #sum(prediction==label_index)会生成0-1矩阵，sum求和就是统计为True的过程，再除以本次batch的数量
+            
+            #sum(prediction==label_index)会生成0-1矩阵，sum求和就是统计为True的过程，再除以本次batch的数量
             acc=torch.sum(prediction==label_index)/data.shape[0]
 
-                #计算损失
+            #计算损失
             loss=criterion(outputs,label.float())
 
             '''
@@ -584,11 +627,10 @@ if __name__=='__main__':
             loss.backward()
             optimizer.step()
 
-                #进度条旁边打印说明
+            #进度条旁边打印说明
             processBar.set_description("[%d/%d] Loss : %.8f Acc：%.8f" %(epoch,Epoch,loss,acc))
 
-        print("Train model end")
-        
+        #下面的部分是在验证集上进行验证，常常结合早停机制
         model.eval()
         with torch.no_grad():
             total_loss=0
@@ -596,11 +638,11 @@ if __name__=='__main__':
             for index,(t_data,t_label) in enumerate(vali_loader):
                 #以下这些和训练的时候一样，可以看上面的训练
                 t_data=t_data.reshape(t_data.shape[0],t_data.shape[1],1).float().to(device)
-                #print("t_data",t_data.shape)
+
                 t_label=t_label.to(device)
-                #print("t_label",t_label.shape)
+  
                 t_outputs=model(t_data)
-                #print("t_outputs",t_outputs.shape)
+
                 loss=criterion(t_outputs,t_label.float())
             
                 t_prediction=torch.argmax(t_outputs,dim=1)
